f14c098531d99d17a9bfba701017d72756768e7c
132yse <1533540012@qq.com>
2019-11-09 10:08:47 +0800

    搞定channel

--- src/channel.js
+++ src/channel.js
@@ -1,65 +1,63 @@
-function channel (callback) {
-  var postToMain = null
-  var postToWorker = null
-  var mainQueue = []
-  var workerQueue = []
-  var messageHandlers = {}
+let postToMain = null
+let postToWorker = null
+let mainQueue = []
+let workerQueue = []
+let messageHandlers = {}
 
-  function connectMain (postMessage) {
-    postToWorker = postMessage
+function connectMain (postMessage, callback) {
+  postToWorker = postMessage
 
-    if (typeof callback === 'function') {
-      callback('main')
-    }
-
-    while (workerQueue.length) {
-      postToWorker(workerQueue.shift())
-    }
-
-    workerQueue = null
+  if (typeof callback === 'function') {
+    callback('main')
   }
 
-  function connectWorker (postMessage) {
-    postToMain = postMessage
-
-    if (typeof callback === 'function') {
-      callback('worker')
-    }
-
-    while (mainQueue.length) {
-      postToMain(mainQueue.shift())
-    }
-
-    mainQueue = null
+  while (workerQueue.length) {
+    postToWorker(workerQueue.shift())
   }
 
-  function postMessageToMain (message) {
-    if (typeof postToMain === 'function') {
-      return postToMain(message)
-    }
-
-    mainQueue.push(message)
-  }
-
-  function postMessageToWorker (message) {
-    if (typeof postToWorker === 'function') {
-      return postToWorker(message)
-    }
-
-    workerQueue.push(message)
-  }
-
-  function handleMessage (type, data) {
-    var messageHandler = messageHandlers[type]
-
-    if (messageHandler != null) {
-      messageHandler(data)
-    }
-  }
-
-  function on (type, messageHandler) {
-    messageHandlers[type] = messageHandler
-  }
-
-  return { connectMain, connectWorker, handleMessage, postMessageToMain, postMessageToWorker, on }
+  workerQueue = null
 }
+
+function connectWorker (postMessage) {
+  postToMain = postMessage
+
+  if (typeof callback === 'function') {
+    callback('worker')
+  }
+
+  while (mainQueue.length) {
+    postToMain(mainQueue.shift())
+  }
+
+  mainQueue = null
+}
+
+function postMessageToMain (message) {
+  if (typeof postToMain === 'function') {
+    return postToMain(message)
+  }
+
+  mainQueue.push(message)
+}
+
+function postMessageToWorker (message) {
+  if (typeof postToWorker === 'function') {
+    return postToWorker(message)
+  }
+
+  workerQueue.push(message)
+}
+
+function handleMessage (type, data) {
+  var messageHandler = messageHandlers[type]
+
+  if (messageHandler != null) {
+    messageHandler(data)
+  }
+}
+
+function on (type, messageHandler) {
+  messageHandlers[type] = messageHandler
+}
+
+return { connectMain, connectWorker, handleMessage, postMessageToMain, postMessageToWorker, on }
