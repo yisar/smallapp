ca83e92785efdf86e9ec205ea01bb265e167c951
132yse <1533540012@qq.com>
2019-11-09 22:08:43 +0800

    日常提交

--- src/master.js
+++ src/master.js
@@ -4,15 +4,14 @@ import channel from './channel'
 export function app (config) {
   let setup = config.setup
 
-  return delegator(new Worker(getPathname()),channel,setup)
+  return delegator(new Worker(getPathname()), channel, setup)
 }
 
-function delegator(worker,channel,callback){
-
-
+function delegator (worker, channel, callback) {
+  
 }
 
-function getPathname() {
+function getPathname () {
   var scripts = document.getElementsByTagName('script')
   return scripts[scripts.length - 1].src
 }
