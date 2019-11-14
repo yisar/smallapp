e6b5e6558045e80023cdcffccbf1b2a467a81c8a
132yse <1533540012@qq.com>
2019-11-14 15:32:13 +0800

    抽象逻辑

--- src/slave.js
+++ src/slave.js
@@ -11,19 +11,21 @@ export function masochism () {
 
   worker = new Worker(PATHNAME)
   worker.onmessage = e => {
-    const commit = e.data
-    commit.forEach(op => {
-      elementMap[op[1]].innerHTML = '' // 暂时清除
-      elementMap.length = 1
-      if (op.length > 3) {
-        if (op[2]) {
-          elementMap[op[1]][op[0]](elementMap[op[3]], elementMap[op[2]])
-        } else {
-          elementMap[op[1]][op[0]](createElement(op[3]), null)
-        }
-      } else {
-        elementMap[op[1]][op[0]](elementMap[op[2]])
-      }
-    })
+    const commitQueue = e.data
+    commitQueue.forEach(commit)
+  }
+}
+
+function commit (op) {
+  elementMap[op[1]].innerHTML = '' // 暂时清除
+  elementMap.length = 1
+  if (op.length > 3) {
+    if (op[2]) {
+      elementMap[op[1]][op[0]](elementMap[op[3]], elementMap[op[2]])
+    } else {
+      elementMap[op[1]][op[0]](createElement(op[3]), null)
+    }
+  } else {
+    elementMap[op[1]][op[0]](elementMap[op[2]])
   }
 }
