ac2d5ea6103969f61ff151f69ffa60c615dee43a
132yse <1533540012@qq.com>
2019-11-14 15:10:09 +0800

    搞定diff思路

--- src/slave.js
+++ src/slave.js
@@ -1,22 +1,22 @@
 import { createElement } from './dom'
-const parentMap = []
+const dom = []
 
 export function masochism () {
   const PATHNAME = (function () {
     const scripts = document.getElementsByTagName('script')
     return scripts[scripts.length - 1].src
   })()
-  parentMap.push(document.body)
+  dom.push(document.body)
 
   const worker = new Worker(PATHNAME)
   worker.onmessage = e => {
     const commit = e.data
     commit.forEach(op => {
-      parentMap[op[1]].innerHTML = '' // 暂时清除
+      dom[op[1]].innerHTML = '' // 暂时清除
       if (op.length > 3) {
-        parentMap[op[1]][op[0]](createElement(op[3], worker), op[2])
+        dom[op[1]][op[0]](createElement(op[3], worker), op[2])
       } else {
-        parentMap[op[1]][op[0]](op[2])
+        dom[op[1]][op[0]](dom[op[2]])
       }
     })
   }
