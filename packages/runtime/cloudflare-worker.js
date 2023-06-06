fd582335d31837fca2d0dc0c2b888702e6ca409f
unknown <1533540012@qq.com>
2023-06-06 15:35:14 +0800

    日常提交

--- packages/runtime/cloudflare-worker.js
+++ packages/runtime/cloudflare-worker.js
@@ -3,16 +3,23 @@ import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 async function handler(req) {
     const { pathname } = new URL(req.url)
 
-    
+
     const entries = [];
     for await (const entry of Deno.readDir(`./packages/runtime/dist`)) {
         entries.push(entry);
     }
 
+    const file = entries.find(i => i.name === pathname.slice(1))
+
+    const FILE_URL = new URL(`./packages/runtime/dist/${file}`, import.meta.url).href;
+
+    const resp = await fetch(FILE_URL);
+
+
     // Return JSON.
-    return new Response(JSON.stringify(entries, null, 2), {
+    return new Response(resp.body, {
         headers: {
-            "content-type": "application/json",
+            "content-type": "text/html",
         },
     });
 }
