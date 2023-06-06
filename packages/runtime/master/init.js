a9c459982c903e9d6980cad650634ec959d09655
unknown <1533540012@qq.com>
2023-06-06 17:27:59 +0800

    日常提交

--- packages/runtime/master/init.js
+++ packages/runtime/master/init.js
@@ -31,7 +31,7 @@ export function init(location) {
 
     style.innerHTML = str
 
-    document.head.appendChild(style)
+    document.body.appendChild(style)
 
     render(h(c, { data: page.data }), document.body)
 }
