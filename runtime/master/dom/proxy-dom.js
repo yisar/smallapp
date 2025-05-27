62cf2681e84e42b95cefc89753c01aa324a2491f
yisar <1533540012@qq.com>
2025-05-27 10:50:34 +0800

    add proxy dom

---
+++ runtime/master/dom/proxy-dom.js
@@ -0,0 +1,197 @@
+if (!self.master) self.master = {}
+
+const getRes = new Map()
+const flushRes = new Map()
+const queue = []
+let dirty = false
+let flushid = 0
+
+let callbackid = 0
+let returnid = 1
+const callbackToId = new Map()
+const idToCallback = new Map()
+
+master.targetSymbol = Symbol('target')
+master.objectSymbol = Symbol('object')
+
+master.getReturnId = () => {
+  return returnid++
+}
+
+function canClone(o) {
+  const t = typeof o
+  return (
+    t === 'undefined' ||
+    o === null ||
+    t === 'boolean' ||
+    t === 'number' ||
+    t === 'string' ||
+    o instanceof Blob ||
+    o instanceof ArrayBuffer ||
+    o instanceof ImageData
+  )
+}
+
+master.enqueue = function (d) {
+  queue.push(d)
+  if (!dirty) {
+    dirty = true
+    Promise.resolve().then(master.flush)
+  }
+}
+
+master.onmessage = function (data) {
+  switch (data.type) {
+    case 'done':
+      done(data)
+      break
+    case 'cb':
+      callback(data)
+      break
+    default:
+      throw new Error('invalid message type: ' + data.type)
+  }
+}
+
+function done(data) {
+  for (const [getId, valueData] of data.res) {
+    const resolve = getRes.get(getId)
+    if (!resolve) throw new Error('invalid get id')
+    getRes.delete(getId)
+    resolve(master.unwrap(valueData))
+  }
+  const flushId = data.flushId
+  const flushResolve = flushRes.get(flushId)
+  if (!flushResolve) throw new Error('invalid flush id')
+
+  flushRes.delete(flushId)
+  flushResolve()
+}
+
+function callback(data) {
+  const fn = idToCallback.get(data.id)
+  if (!fn) throw new Error('invalid callback id')
+  const args = data.args.map(master.unwrap)
+  fn(...args)
+}
+
+master.unwrap = function (arr) {
+  switch (arr[0]) {
+    case 0:
+      return arr[1]
+    case 1:
+      return master.makeObj(arr[1])
+    default:
+      throw new Error('invalid arg type')
+  }
+}
+
+master.flush = function () {
+  dirty = false
+  if (!queue.length) return Promise.resolve()
+  const flushId = flushid++
+
+  master.postMessage({
+    type: 'cmds',
+    cmds: queue,
+    flushId: flushId,
+  })
+
+  queue.length = 0
+  return new Promise((resolve) => {
+    flushRes.set(flushId, resolve)
+  })
+}
+
+master.wrap = function (arg) {
+  if (typeof arg === 'function') {
+    const objectId = arg[master.objectSymbol]
+    if (typeof objectId === 'number') return [1, objectId]
+    const target = arg[master.targetSymbol]
+    if (target) {
+      return [3, target.id, target.path]
+    }
+    return [2, getCid(arg)]
+  } else if (canClone(arg)) {
+    return [0, arg]
+  } else throw new Error('invalid argument')
+}
+
+function getCid(fn) {
+  let id = callbackToId.get(fn)
+  if (typeof id === 'undefined') {
+    id = callbackid++
+    callbackToId.set(fn, id)
+    idToCallback.set(id, fn)
+  }
+  return id
+}
+
+const objHandler = {
+  get(target, property) {
+    if (property === master.objectSymbol) return target.id
+    return master.makeProp(target.id, [property])
+  },
+  set(target, property, value) {
+    master.enqueue([1, target.id, [property], master.wrap(value)])
+    return true
+  },
+}
+
+const propHandler = {
+  get(target, property) {
+    if (property === master.targetSymbol) return target
+    const cache = target.cache
+    const c = cache.get(property)
+    if (c) return c
+    const path = target.path.slice(0)
+    path.push(property)
+    const ret = master.makeProp(target.id, path)
+    cache.set(property, ret)
+    return ret
+  },
+
+  set(target, property, value) {
+    const path = target.path.slice(0)
+    path.push(property)
+    master.enqueue([1, target.id, path, master.wrap(value)])
+    return true
+  },
+
+  apply(target, thisArg, args) {
+    const returnid = master.getReturnId()
+    master.enqueue([0, target.id, target.path, args.map(master.wrap), returnid])
+    return master.makeObj(returnid)
+  },
+
+  construct(target, args) {
+    const returnid = master.getReturnId()
+    master.enqueue([2, target.id, target.path, args.map(master.wrap), returnid])
+    return master.makeObj(returnid)
+  },
+}
+
+master.makeObj = function (id) {
+  const fn = function () {}
+  fn.id = id
+  return new Proxy(fn, objHandler)
+}
+
+master.makeProp = function (id, path) {
+  const fn = function () {}
+  fn.id = id
+  fn.path = path
+  fn.cache = new Map()
+  return new Proxy(fn, propHandler)
+}
+
+self.effect = (cb) => {
+  self.addEventListener('message', (e) => {
+    if (e.data === 'start') {
+      master.postMessage = (data) => self.postMessage(data)
+      cb(master.makeObj(0))
+    } else {
+      master.onmessage(e.data)
+    }
+  })
+}
---
+++ runtime/slave/proxy.js
@@ -0,0 +1,164 @@
+if (!self.slave) self.slave = {}
+const idMap = new Map([[0, self]])
+let nextid = -1
+
+document.define = (tag, mount, unmount) => {
+    let host = null
+    customElements.define(
+        tag,
+        class extends HTMLElement {
+            constructor() {
+                super()
+                host = this.attachShadow({ mode: 'open' })
+            }
+            connectedCallback() {
+                mount()
+            }
+            disconnectedCallback() {
+                unmount()
+            }
+        }
+    )
+    return host
+}
+
+slave.wrap = (arg) => {
+    if (canClone(arg)) {
+        return [0, arg]
+    } else {
+        return [1, obj2id(arg)]
+    }
+}
+
+slave.unwrap = (arr) => {
+    switch (arr[0]) {
+        case 0: // primitive
+            return arr[1]
+        case 1: // object
+            return id2obj(arr[1])
+        case 2: // callback
+            return getCb(arr[1])
+        case 3: // property
+            return id2prop(arr[1], arr[2])
+        default:
+            throw new Error('invalid arg type')
+    }
+}
+
+slave.connect = function connect(worker) {
+    worker.onmessage = (e) => slave.onmessage(e.data)
+    slave.postMessage = (data) => worker.postMessage(data)
+    worker.postMessage('start')
+}
+
+function id2obj(id) {
+    const ret = idMap.get(id)
+    if (!ret) throw new Error('missing object id: ' + id)
+    return ret
+}
+
+function obj2id(object) {
+    const id = nextid--
+    idMap.set(id, object)
+    return id
+}
+
+function id2prop(id, path) {
+    const ret = idMap.get(id)
+    if (!ret) throw new Error('missing object id: ' + id)
+    let base = ret
+    for (let i = 0, len = path.length; i < len; ++i) base = base[path[i]]
+    return base
+}
+
+function getCb(id) {
+    return (...args) =>
+        slave.postMessage({
+            type: 'cb',
+            id,
+            args: args.map(slave.wrap),
+        })
+}
+
+slave.onmessage = function (data) {
+    switch (data.type) {
+        case 'cmds':
+            command(data)
+            break
+        case 'cleanup':
+            cleanup(data)
+            break
+        default:
+            console.error('Unknown message type: ' + data.type)
+            break
+    }
+}
+
+function command(data) {
+    const res = []
+    for (const cmd of data.cmds) process(cmd, res)
+
+    slave.postMessage({
+        type: 'done',
+        flushId: data.flushId,
+        res,
+    })
+}
+
+function process(arr) {
+    const type = arr[0]
+    switch (type) {
+        case 0:
+            call(arr[1], arr[2], arr[3], arr[4], type)
+            break
+        case 1:
+            set(arr[1], arr[2], arr[3])
+            break
+        case 2:
+            call(arr[1], arr[2], arr[3], arr[4], type)
+            break
+        default:
+            throw new Error('invalid cmd type: ' + type)
+    }
+}
+
+function call(id, path, arg, returnid, isNew) {
+    const obj = id2obj(id)
+    const args = arg.map(slave.unwrap)
+    const name = path[path.length - 1]
+    let base = obj
+    for (let i = 0; i < path.length - 1; i++) {
+        base = base[path[i]]
+    }
+    let ret = isNew ? new base[name](...args) : base[name](...args)
+    idMap.set(returnid, ret)
+}
+
+function set(id, path, arg) {
+    const obj = id2obj(id)
+    const value = slave.unwrap(arg)
+    const name = path[path.length - 1]
+    let base = obj
+    for (let i = 0; i < path.length - 1; i++) {
+        base = base[path[i]]
+    }
+    base[name] = value
+}
+
+function cleanup(data) {
+    for (const id of data.ids) idMap.delete(id)
+}
+
+function canClone(o) {
+    const t = typeof o
+    return (
+        t === 'undefined' ||
+        o === null ||
+        t === 'boolean' ||
+        t === 'number' ||
+        t === 'string' ||
+        o instanceof Blob ||
+        o instanceof ArrayBuffer ||
+        o instanceof ImageData
+    )
+}
